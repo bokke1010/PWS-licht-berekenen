f1069a4f2ec22e470372b6aaa327acf3934bcce8
Bokke <bokke1010@gmail.com>
2019-12-29 13:52:39 +0100

    Remove Globe class Add timeSteps variable Minor optimizations

--- 2D_plotter.py
+++ 2D_plotter.py
@@ -2,28 +2,27 @@ from PIL import Image
 import calculator, os
 from multiprocessing import pool
 
-def calculatexyt(globe, x, y, w, h, t, a):
-    fraction = globe.calculatePoint(timeOfDay = x/w - 0.25, latitude = 2 * y/h - 1, inclination=4/9, timeOfYear=t/a)
-    return t, x, y , int((fraction)*255),0,0,int(fraction*128)+127
+def calculatexyt(x, y, w, h, t, a):
+    fraction = calculator.calculatePoint(longitude = 2*x/w - 0.25, latitude = 2 * y/h - 1, inclination=8/9, timeOfDay=t/a)
+    return t, x, y , (int((fraction)*255),0,0,int(fraction*128)+127)
 
 
 if __name__ == "__main__":
     P = pool.Pool(processes=12)
     width, height = 320, 320
-    globe = calculator.Globe(False)
-    points = [(globe, x, y, width, height, t, 8) for t in range(8) for x in range(width) for y in range(height)]
+    timeSteps = 16
+    points = [(x, y, width, height, t, timeSteps) for t in range(timeSteps) for x in range(width) for y in range(height)]
     data = P.starmap(calculatexyt, points)
     P.terminate()
     print("c")
     im, px = {}, {}
-    for t in range(8):
+    for t in range(timeSteps):
         im[t] = Image.new("RGBA",(width, height))
         px[t] = im[t].load()
     for item in data:
-        t, x, y, *color = item
-        color = tuple(color)
+        t, x, y, color = item
         px[t][x,y] = color
-    for t in range(8):
+    for t in range(timeSteps):
         im[t].save(f"year/result{t}.png")
     print("d")
     exit()
--- calculator.py
+++ calculator.py
@@ -3,8 +3,8 @@ from math import pi, sin, cos
 from scipy import spatial
 import numpy as np
 
-def distance(*args):
-    return sum(map(lambda n: n**2, args))**0.5
+def distanceCubed(*args):
+    return sum(map(lambda n: n**2, args))
 
 class location():
     def __init__(self, x, y, z=None):
@@ -34,52 +34,43 @@ class location():
         return self
 
 
-class Globe():
-    def __init__(self, useZ = True):
-        self.useZ = useZ
 
 
-    def calculateOnPlane(self, pos: location):
-        deviation = distance(*pos)
-        if deviation <= 1:
-            return math.cos(distance(deviation) * math.pi * 0.5)
-        return 0
 
-    def calculateOnGlobe(self, pos: location):
-        # This code assumes a xy-plane perpendicular to the light source, with
-        # the z-axis's positive side facing the light source (negative z is the
-        # opposite side of the globe)
-        if pos.z < 0 and self.useZ:
-            return 0
-        return self.calculateOnPlane(pos.to2D())
+def calculateOnPlane(pos: location):
+    deviation = distanceCubed(*pos)
+    if deviation <= 1:
+        return cos(deviation**0.5 * pi * 0.5)
+    return 0
 
-    def calculatePointIncl0(self, timeOfDay, latitude):
-        """At time=0, it is morning, at time = 0.25, it is noon etc."""
-        x= cos(pi * latitude * 0.5) * cos(pi * timeOfDay * 2)
-        y= sin(pi * latitude * 0.5)
-        z= cos(pi * latitude * 0.5) * sin(pi * timeOfDay * 2)
-        return self.calculateOnGlobe(location(x,y,z))
+def calculateOnGlobe(pos: location):
+    # This code assumes a xy-plane perpendicular to the light source, with
+    # the z-axis's positive side facing the light source (negative z is the
+    # opposite side of the globe)
+    return calculateOnPlane(pos.to2D())
 
-    def calculatePoint(self, timeOfDay = 0, timeOfYear = 0, inclination = 0, latitude = 0):
-        """Calculates the relative amount of light a point on a globe recieves
-        on a time of day (0,1) at a time of the year (0,1) on a given latitude
-        (-1,1), on a globe with a specific inclination (0,1)
-        """
-        # rotation vector:
-        # x= cos(pi * timeOfYear * 2) * 0.5 * pi * inclination
-        # y= sin(pi * timeOfYear *2) * 0.5* pi * inclination
-        # z= 0
-        locVec = np.array([
-            cos(pi * latitude * 0.5) * cos(pi * timeOfDay * 2),
-            sin(pi * latitude * 0.5),
-            cos(pi * latitude * 0.5) * sin(pi * timeOfDay * 2)])
-        rotAngle = 0.5 * pi * inclination
-        yearPerpAngle = pi * (timeOfYear+0.5) * 2
-        rotVec = np.array([cos(yearPerpAngle) * rotAngle, sin(yearPerpAngle) * rotAngle, 0])
-        rotationVector = spatial.transform.Rotation.from_rotvec(rotVec)
-        rotatedVector = rotationVector.apply(locVec)
+def calculatePoint(timeOfDay = 0, timeOfYear = 0, inclination = 0, latitude = 0, longitude = 0):
+    """Calculates the relative amount of light a point on a globe recieves
+    on a time of day (0,1) at a time of the year (0,1) on a given latitude
+    and longtude (-1,1), on a globe with a specific inclination (0,1)
+    """
+    # rotation vector:
+    # x= cos(pi * timeOfYear * 2) * 0.5 * pi * inclination
+    # y= sin(pi * timeOfYear *2) * 0.5* pi * inclination
+    # z= 0
+    localLongitudeAngle = pi * (longitude + timeOfDay * 2)
+    a = cos(pi * latitude * 0.5)
+    locVec = np.array([
+        a * cos(localLongitudeAngle),
+        sin(pi * latitude * 0.5),
+        a * sin(localLongitudeAngle) ])
+    rotAngle = 0.5 * pi * inclination
+    yearPerpAngle = pi * (timeOfYear+0.5) * 2
+    rotVec = np.array([cos(yearPerpAngle) * rotAngle, sin(yearPerpAngle) * rotAngle, 0])
+    rotationVector = spatial.transform.Rotation.from_rotvec(rotVec)
+    rotatedVector = rotationVector.apply(locVec)
 
-        return self.calculateOnGlobe(location(*rotatedVector))
+    return calculateOnGlobe(location(*rotatedVector))
 
 
 # globe = Globe()
